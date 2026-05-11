7f44358f0df132d244715d46fdb7f9a6cde9b5c8
kamalarkaj11 <kamalarkaj11@gmail.com>
2026-05-11 21:51:20 +0530

    Added grocery_store as normal folder

---
+++ grocery_store/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for grocery_store project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/specifications/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_store.settings')
+
+application = get_asgi_application()
+
