f6edc1c34c900be95bf9f55569228934fedd9ace
kamalarkaj11 <kamalarkaj11@gmail.com>
2026-06-12 21:50:56 +0530

    Main

--- grocery_store/settings.py
+++ grocery_store/settings.py
@@ -182,7 +182,7 @@ GROCERY_API_FORBIDDEN_PAUSE_SECONDS = int(os.getenv('GROCERY_API_FORBIDDEN_PAUSE
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '25'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
